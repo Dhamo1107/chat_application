617ed0a068079698694dd8841360d363ebe7da1f
DHAMODHARAN S <dhamodharan.senkottaiyan@mallow-tech.com>
2023-03-07 20:00:36 +0530

    Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,20 @@
 class UsersController < ApplicationController
   def index
-
     if params[:search].present?
-      @users = User.where("email LIKE ?", "#{params[:search]}%").where.not(id: current_user.id)
+      @users = User.where("email LIKE ?", "#{params[:search]}%").where.not(id: current_user.id).order(:email)
     else
-      @users = User.where.not(id: current_user.id)
+      @users = User.where.not(id: current_user.id).order(:email)
     end
+    @users = @users.paginate(page: params[:page], per_page: 8)
   end
 
   def chat
     @user = User.find(params[:id])
-    @messages = current_user.sent_messages.where(receiver_id: @user.id).or(current_user.received_messages.where(sender_id: @user.id)).order(created_at: :asc)
+    @messages = current_user.sent_messages
+                            .where(receiver_id: @user.id)
+                            .or(current_user.received_messages.where(sender_id: @user.id))
+                            .includes(:sender)
+                            .order(created_at: :asc)
     @message = Message.new
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to :sender, class_name: "User", counter_cache: :sent_messages_count
-  belongs_to :receiver, class_name: "User", counter_cache: :received_messages_count
-
+  belongs_to :sender, class_name: "User"
+  belongs_to :receiver, class_name: "User"
+  validates :content, presence: true
 end
