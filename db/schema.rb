364f2849a8d6d27e161c51da66af4d84b4795f8e
Dhamo1107 <dhamodharansathish4533@gmail.com>
2024-06-22 19:28:59 +0530

    Added package files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_080338) do
+ActiveRecord::Schema.define(version: 2023_03_01_080444) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.integer "sender_id"
@@ -29,8 +32,6 @@ ActiveRecord::Schema.define(version: 2023_03_02_080338) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.integer "sent_messages_count", default: 0
-    t.integer "received_messages_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
